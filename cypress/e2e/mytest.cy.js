83eb614aa02c1aa2b59f70d0fda0fe7f20d1aeea
anbudeen <anbu.devai87@gmail.com>
2025-05-29 18:46:57 +0530

    Added GitHub Action to run regression tests

--- cypress/e2e/mytest.cy.js
+++ cypress/e2e/mytest.cy.js
@@ -1,5 +1,6 @@
 describe("Login",()=>{
     it("Validation of user login",()=>{
         cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        cy.title().should('contain','OrangeHRM')
     })
 })
